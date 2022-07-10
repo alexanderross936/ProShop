d3604f1c609b11900394b754052a38faf4c97cd5
Ross <alexander.ross@definitivelogic.com>
2022-07-09 19:30:16 -0500

    master branch

--- models/User.js
+++ models/User.js
@@ -20,6 +20,7 @@ const orderSchema = mongoose.Schema(
         },
       },
     ],
+
     shippingAddress: {
       address: { type: String, required: true },
       city: { type: String, required: true },
